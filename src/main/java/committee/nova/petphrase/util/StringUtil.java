e1fb41ae442a08c7959477fd8deb1679ede088d0
MikhailTapio <mikhailtapio13468@163.com>
2022-08-01 18:58:04 +0800

    Port to Fabric 1.18.X-1.19.X

---
+++ src/main/java/committee/nova/petphrase/util/StringUtil.java
@@ -0,0 +1,26 @@
+package committee.nova.petphrase.util;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.List;
+
+public class StringUtil {
+    static final List<Character> punctuations = List.of('!', '?', '.', '(', ')', '！', '？', '。', '（', '）', '~', '”', '“', '‘', '’', '"', '\'');
+
+    public static int getLastPunc(String string) {
+        final int length = string.length();
+        if (length == 1) return (punctuations.contains(string.charAt(0))) ? -1 : 0;
+        return getPunc(string, length - 1);
+    }
+
+    public static int getPunc(String string, int index) {
+        return (!punctuations.contains(string.charAt(index))) ? index : (index == 0) ? -1 : getPunc(string, index - 1);
+    }
+
+    public static String fillPetPhraseIn(String original, String petPhrase) {
+        if (original.charAt(0) == '/') return original;
+        final int index = getLastPunc(original) + 1;
+        if (index == 0) return original;
+        return StringUtils.substring(original, 0, index) + petPhrase + StringUtils.substring(original, index);
+    }
+}
